00438213d1c9c7075fca47331f1611b274827bd1
Alexanndre85DF <tolentinoalexandre534@gmail.com>
2025-07-05 23:47:33 -0300

    Corrige indentação final da função index

--- app.py
+++ app.py
@@ -117,13 +117,13 @@ def index():
         prof = cur.fetchone()
 conn.close()
 
-if prof:
-    session['professor_id'] = prof[0]
-    session['professor_email'] = prof[1]
-    if prof[1] == '01099080150' and prof[2] == 'brasilia85DF':
-        session['is_admin'] = True
-    return redirect(url_for('dashboard', professor_id=prof[0]))
-else:
+    if prof:
+        session['professor_id'] = prof[0]
+        session['professor_email'] = prof[1]
+        if prof[1] == '01099080150' and prof[2] == 'brasilia85DF':
+            session['is_admin'] = True
+        return redirect(url_for('dashboard', professor_id=prof[0]))
+    else:
         flash("Login inválido!", "danger")
         return redirect(url_for('index'))
 
